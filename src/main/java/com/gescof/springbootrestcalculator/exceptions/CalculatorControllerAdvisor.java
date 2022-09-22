4299136d0ef055e14b8faedfa1c37c98f769a975
Gescof <gescolarfernandez@gmail.com>
2022-09-22 22:43:07 +0200

    Add log details to CalculatorControllerAdvisor and tests

--- src/main/java/com/gescof/springbootrestcalculator/exceptions/CalculatorControllerAdvisor.java
+++ src/main/java/com/gescof/springbootrestcalculator/exceptions/CalculatorControllerAdvisor.java
@@ -4,19 +4,23 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.Clock;
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Log4j2
 @RequiredArgsConstructor
-@ControllerAdvice
+@RestControllerAdvice
 public class CalculatorControllerAdvisor extends ResponseEntityExceptionHandler {
     private final Clock clock;
     private static final String NO_VALID_FORMAT_MESSAGE = "Input has no valid format";
@@ -24,14 +28,24 @@ public class CalculatorControllerAdvisor extends ResponseEntityExceptionHandler 
 
     @ExceptionHandler(NumberFormatException.class)
     public ResponseEntity<Object> handleInvalidInputFormat(NumberFormatException ex, WebRequest request) {
-        log.warn(String.format("Handling invalid input format for request %s", request.getDescription(true)));
+        log.warn(String.format("Handling invalid input format for request %s [Input query params: %s]",
+                Objects.nonNull(request) ? request.getDescription(true) : "",
+                getQueryParamsMapString(Objects.nonNull(request) ? request.getParameterMap() : new HashMap<>())));
         return getConflictResponseEntity(NO_VALID_FORMAT_MESSAGE, ex.getMessage());
     }
 
     @ExceptionHandler(ArithmeticException.class)
     public ResponseEntity<Object> handleArithmeticException(ArithmeticException ex, WebRequest request) {
-        log.warn(String.format("Handling arithmetic exception for request %s", request.getDescription(true)));
+        log.warn(String.format("Handling arithmetic exception for request %s [Input query params: %s]",
+                Objects.nonNull(request) ? request.getDescription(true) : "",
+                getQueryParamsMapString(Objects.nonNull(request) ? request.getParameterMap() : new HashMap<>())));
         return getConflictResponseEntity(ARITHMETIC_ERROR_MESSAGE, ex.getMessage());
+    }
+
+    private static String getQueryParamsMapString(Map<String, String[]> queryParamsMap) {
+        return queryParamsMap.keySet().stream()
+                .map(key -> key + "=" + Arrays.toString(queryParamsMap.get(key)))
+                .collect(Collectors.joining(", ", "{", "}"));
     }
 
     private ResponseEntity<Object> getConflictResponseEntity(String message, String errors) {
---
+++ src/test/java/com/gescof/springbootrestcalculator/exceptions/CalculatorControllerAdvisorTest.java
@@ -0,0 +1,74 @@
+package com.gescof.springbootrestcalculator.exceptions;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Map;
+
+public class CalculatorControllerAdvisorTest {
+    private final static LocalDateTime LOCAL_DATE_TIME = LocalDateTime.of(
+            LocalDate.of(2021, 10, 23),
+            LocalTime.of(12, 35, 59, 0)
+    );
+    @InjectMocks
+    private CalculatorControllerAdvisor calculatorControllerAdvisor;
+
+    @Mock
+    private Clock mockedClock;
+
+    private Clock fixedClock;
+
+    @BeforeEach
+    void init() {
+        MockitoAnnotations.openMocks(this);
+        fixedClock = Clock.fixed(LOCAL_DATE_TIME.atZone(ZoneId.systemDefault()).toInstant(), ZoneId.systemDefault());
+        Mockito.doReturn(fixedClock.instant()).when(mockedClock).instant();
+        Mockito.doReturn(fixedClock.getZone()).when(mockedClock).getZone();
+    }
+
+    @Test
+    void testShouldHandleInvalidInputFormatReturningHttpStatus409() {
+        Map<String, Object> expectedResultBody = Map.of(
+                "timestamp", LocalDateTime.now(fixedClock),
+                "status", HttpStatus.CONFLICT.toString(),
+                "message", "Input has no valid format",
+                "errors", new NumberFormatException("Detail message").getMessage());
+        ResponseEntity<Object> invalidInputFormatResult = calculatorControllerAdvisor.handleInvalidInputFormat(
+                new NumberFormatException("Detail message"),
+                null
+        );
+        Assertions.assertEquals(
+                new ResponseEntity<>(expectedResultBody, HttpStatus.CONFLICT),
+                invalidInputFormatResult
+        );
+    }
+
+    @Test
+    void testShouldHandleArithmeticExceptionReturningHttpStatus409() {
+        Map<String, Object> expectedResultBody = Map.of(
+                "timestamp", LocalDateTime.now(fixedClock),
+                "status", HttpStatus.CONFLICT.toString(),
+                "message", "Error in arithmetic conditions",
+                "errors", new ArithmeticException("Detail message").getMessage());
+        ResponseEntity<Object> arithmeticExceptionResult = calculatorControllerAdvisor.handleArithmeticException(
+                new ArithmeticException("Detail message"),
+                null
+        );
+        Assertions.assertEquals(
+                new ResponseEntity<>(expectedResultBody, HttpStatus.CONFLICT),
+                arithmeticExceptionResult
+        );
+    }
+}
