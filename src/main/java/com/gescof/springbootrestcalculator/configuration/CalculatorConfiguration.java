65cf89d94c341457371bb0835267593c3fa6b723
Gescof <gescolarfernandez@gmail.com>
2022-11-02 19:42:20 +0100

    Add Cacheable to GET operations method

--- src/main/java/com/gescof/springbootrestcalculator/configuration/CalculatorConfiguration.java
+++ src/main/java/com/gescof/springbootrestcalculator/configuration/CalculatorConfiguration.java
@@ -1,11 +1,15 @@
 package com.gescof.springbootrestcalculator.configuration;
 
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 import java.time.Clock;
 
 @Configuration
+@EnableCaching
+@EnableMongoAuditing
 public class CalculatorConfiguration {
     @Bean
     public Clock clock() {
